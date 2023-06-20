5bb75a56a7ba2fdb8c326cf9ff97745b88d3b51e
Panzerwae <kpjventer@gmail.com>
2023-06-20 14:12:05 +0200

    Initial Commit for Game Hub Web

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { environment } from '../environments/environment';
+
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyCe65PE7sbQejcUZY4GDVX9umudWDFyG-w",
+        authDomain: "game-hub-e7d9f.firebaseapp.com",
+        projectId: "game-hub-e7d9f",
+        storageBucket: "game-hub-e7d9f.appspot.com",
+        messagingSenderId: "286214731330",
+        appId: "1:286214731330:web:ee6012a20c60c6b7e41fdb",
+        measurementId: "G-LW3B0S667Q"
+    }
+};
